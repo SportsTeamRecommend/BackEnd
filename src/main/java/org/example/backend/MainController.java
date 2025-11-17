50961c9ab2bdd97b8334946be64027e667dad379
jihwanyeom <yum020428@gmail.com>
2025-11-17 14:19:18 +0900

    feat : kbo 추천 api 수정

--- src/main/java/org/example/backend/MainController.java
+++ src/main/java/org/example/backend/MainController.java
@@ -5,6 +5,7 @@ import org.example.backend.baseball.table.KboWeight;
 import org.example.backend.baseball.team.Region;
 import org.example.backend.baseball.weight.KboWeightRepository;
 import org.example.backend.common.weight.dto.F1RecommendResponse;
+import org.example.backend.common.weight.dto.KboRecommendResponse;
 import org.example.backend.common.weight.dto.UserF1RecommendRequest;
 import org.example.backend.f1.weight.F1TeamWeight;
 import org.example.backend.baseball.weight.UserKboWeight;
@@ -28,11 +29,11 @@ public class MainController {
     private final KboWeightRepository kboWeightRepository;
 
     @PostMapping("/kbo/recommend")
-    public ResponseEntity<List<Map.Entry<String, Double>>> recommendTeams(@RequestBody UserKboWeight userKboWeight, @RequestParam Region userRegion) {
-        List<KboWeight> allTeams = kboWeightRepository.findAll();
-        List<Map.Entry<String, Double>> result =  kboWeightService.getKboWeightRanks(allTeams, userKboWeight, userRegion);
+    public ResponseEntity<List<KboRecommendResponse>> recommendTeams(@RequestBody UserKboWeight userKboWeight, @RequestParam Region userRegion) {
+        List<KboWeight> teamWeights = kboWeightRepository.findAll();
+        List<KboRecommendResponse> result =  kboWeightService.getKboWeightRanks(teamWeights, userKboWeight, userRegion);
 
-        return ResponseEntity.ok(result);
+        return ResponseEntity.status(200).body(result);
     }
 
     @PostMapping("/f1/recommend")
